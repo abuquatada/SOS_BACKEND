db8220777ea6786676819f94f3f4c0ba34e06c74
Noore Nabi <noorenabi845304@gmail.com>
2024-09-01 23:56:12 +0530

    Apply Filter on All interview Column

--- interviewer/filters.py
+++ interviewer/filters.py
@@ -18,3 +18,20 @@ class InterviewerFilter(django_filters.FilterSet):
     class Meta:
         model = Interviewer
         fields = ['first_name', 'email', 'total_years_of_experience', 'gender']
+
+class InterviewFilter(django_filters.FilterSet):
+    interview_id = django_filters.NumberFilter()
+    application_id = django_filters.NumberFilter(field_name='application_id__application_id')
+    phase = django_filters.CharFilter(field_name='phase__phase_name', lookup_expr='icontains')
+    type = django_filters.ChoiceFilter(choices=[('Onsite', 'Onsite'), ('Virtual', 'Virtual')])
+    scheduled_date = django_filters.DateFilter()
+    interviewer = django_filters.CharFilter(field_name='interviewer__first_name', lookup_expr='icontains')
+    location = django_filters.CharFilter(lookup_expr='icontains')
+    virtual_link = django_filters.CharFilter(lookup_expr='icontains')
+    status = django_filters.CharFilter(lookup_expr='icontains')
+    notes = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Interview
+        fields = ['interview_id', 'application_id', 'phase', 'type', 'scheduled_date', 
+                  'interviewer', 'location', 'virtual_link', 'status', 'notes']
