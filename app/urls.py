714b0665031a0e4bdc734ffbd4d53c82a902dad1
Noore Nabi <noorenabi845304@gmail.com>
2024-09-09 15:42:34 +0530

    Google Form Url Added

--- app/urls.py
+++ app/urls.py
@@ -116,6 +116,10 @@ urlpatterns = [
     path('feedback/<int:pk>/', Interview_Feedback_View),
     path('jobpostingcsv/',JobpostingCSV),
     path('applicant-document/',applicant_document),
-    path('applicant-document/<int:pk>/',applicant_document)
+    path('applicant-document/<int:pk>/',applicant_document),
+    path('google/forms/init/', GoogleFormsInitView.as_view(), name='google_forms_init'),
+    path('google/forms/redirect/', GoogleFormsRedirectView.as_view(), name='google_forms_redirect'),
+    path('google/forms/access/<uuid:token>/', AccessFormView.as_view(), name='access_form'),
+    
 
 ]
